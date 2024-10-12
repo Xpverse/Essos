8973efdfcb5aefaa068eb4671e512685af7ff0ef
Atul Thakur <atulthakur9076@gmail.com>
2024-10-13 00:39:21 +0530

    stage management added - part 3

--- rig-frontend/src/redux/actions/materialRequestsAction.js
+++ rig-frontend/src/redux/actions/materialRequestsAction.js
@@ -17,6 +17,18 @@ export const FETCH_SUCCESS_CURRENT_MATERIALREQUESTITEMS = "FETCH_SUCCESS_CURRENT
 
 export const FETCH_FAILURE_CURRENT_MATERIALREQUESTITEMS = "FETCH_FAILURE_CURRENT_MATERIALREQUESTITEMS"
 
+export const BULK_UPLOAD_MATERIALREQUESTITEMS = "BULK_UPLOAD_MATERIALREQUESTITEMS"
+
+export const BULK_UPLOAD_SUCCESS_MATERIALREQUESTITEMS = "BULK_UPLOAD_SUCCESS_MATERIALREQUESTITEMS"
+
+export const BULK_UPLOAD_FAILURE_MATERIALREQUESTITEMS = "BULK_UPLOAD_FAILURE_MATERIALREQUESTITEMS"
+
+export const POST_DATA_MATERIALREQUEST = "POST_DATA_MATERIALREQUEST"
+
+export const POST_DATA_SUCCESS_MATERIALREQUEST = "POST_DATA_SUCCESS_MATERIALREQUEST"
+
+export const POST_DATA_FAILURE_MATERIALREQUEST = "POST_DATA_FAILURE_MATERIALREQUEST"
+
 export const fetchMaterialRequestDataRequest = () => ({
     type: FETCH_DATA_REQUEST_MATERIALREQUEST
 })
@@ -59,6 +71,33 @@ export const fetchCurrentMaterialRequestItemsFailure = (error) => ({
     payload: error
 })
 
+export const bulkUploadMaterialRequestItems = () => ({
+    type: BULK_UPLOAD_MATERIALREQUESTITEMS
+})
+
+export const bulkUploadMaterialRequestItemsSuccess = (data) => ({
+    type: BULK_UPLOAD_SUCCESS_MATERIALREQUESTITEMS,
+    payload: data
+})
+
+export const bulkUploadMaterialRequestItemsFailure = (error) => ({
+    type: BULK_UPLOAD_FAILURE_MATERIALREQUESTITEMS,
+    payload: error
+})
+
+export const postMaterialRequest = () => ({
+    type: POST_DATA_MATERIALREQUEST
+})
+
+export const postMaterialRequestSuccess = (data) => ({
+    type: POST_DATA_SUCCESS_MATERIALREQUEST,
+    payload: data
+})
+
+export const postMaterialRequestFailure = (error) => ({
+    type: POST_DATA_FAILURE_MATERIALREQUEST,
+    payload: error
+})
 
 
 export const fetchMaterialRequestData = () => {
@@ -117,3 +156,49 @@ export const fetchCurrentMaterialRequestItemsFinalAction = (id) => {
 }
 
 
+
+export const bulkUploadMaterialRequestItemsFinalAction = (file) => {
+    return (dispatch) => {
+        dispatch(bulkUploadMaterialRequestItems())
+        const formData = new FormData();
+        formData.append('file', file);
+
+        axios.post(`http://localhost:8000/api/v1/material-request-items/upload`,formData,{
+            headers: {
+                'Content-Type': 'multipart/form-data',
+              }
+        })
+            .then(response => {
+                const data = response.data
+                console.log(data)
+                //dispatch(bulkUploadMaterialRequestItemsSuccess(data))
+
+            })
+            .catch(error => {
+                console.log(error)
+                //dispatch(bulkUploadMaterialRequestItemsFailure(error))
+            })
+    }
+    
+}
+
+
+export const postMaterialRequestFinalAction = (materialRequest,file) => {
+    return (dispatch) => {
+        dispatch(postMaterialRequest())
+
+        axios.post(`http://localhost:8000/api/v1/material-request`,materialRequest)
+            .then(response => {
+                const data = response.data
+                console.log(data)
+                dispatch(bulkUploadMaterialRequestItemsFinalAction(file))
+                //dispatch(bulkUploadMaterialRequestItemsSuccess(data))
+
+            })
+            .catch(error => {
+                console.log(error)
+                //dispatch(bulkUploadMaterialRequestItemsFailure(error))
+            })
+    }
+    
+}
